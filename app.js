0af53e13a597f6e39295e68014e735e656498abd
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2022-10-21 01:51:44 -0500

    added a css, a js file and content to my about me

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// I declared a variable called answer1 = name
+// Assigned it a string = data type
+// With the value 'Was I born in Colombia or the cayman Islands?'. = value
+let answer1 = prompt('Was I born in Colombia? (y/n or yes/no)');
+
+console.log(answer1);
+
+// // String concatenation
+alert('You answered ' + answer1 + ' as my place of birth');
+    if answer1 === ('yes');
+        alert('That answer is correct')
+
+// let answer2 = prompt('Which sport do you think is my favorite, soccer or basketball?';
+
+// console.log(answer2);
+
+// alert('You think ' + answer2 + ' is my favorite sport');
+
+// if (answer2 === 'soccer') {
+// 	alert('That is correct');
+// };
+
+// let answer3 = prompt('Which do you think is my favorite food? Pizza or Pasta?');
+// console.log(answer3);
+
+// alert('That is interesting that you selected ' + answer3 + ' as my favorite food, because pasta is my favorite food!');
+
+// let answer4 = prompt('My birth month starts with the letter "J" what month is it?');
+// console.log(answer4);
+
+// //Error I encountered in the below line: My entire script was not running because the below line ended with the + answer +. To fix it I had to end it with a single quote.
+// alert('My birth month is July, and your answer was ' + answer4 + ' you also said that I was born in ' + answer1 + ' that my favorite sport is ' + answer2 + ' and that my favorite food is ' + answer3 + '');
+
